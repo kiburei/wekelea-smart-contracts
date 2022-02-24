a1b239a309c6658becb8b2405456dd6f4a59962b
kiburei <gooddeeds17@gmail.com>
2022-02-25 00:04:18 +0300

    creating a user using enum and struct

--- contracts/Users.sol
+++ contracts/Users.sol
@@ -3,14 +3,23 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Users {
 
-  enum Gender{HE, SHE, OTHER}
-  Gender gender;
+  enum Gender { Male, Female, Other}
 
-  function setGender(uint g) public {
-    gender = Gender.HE;
-  }
+    struct AppUser {
+        string first_name;
+        string last_name;
+        string username;
+        Gender gender;
+    }
 
-  constructor() public {
-  }
+    AppUser public app_user;
+
+    function setUpUser(string memory _first_name, string memory _last_name, string memory _username, uint8 _gender) public {
+        app_user = AppUser(_first_name, _last_name, _username, Gender(_gender));
+    }
+
+    function getAppUser() public view returns(AppUser memory) {
+      return app_user;
+    }
 
 }
